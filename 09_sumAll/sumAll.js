b47e115366d2b947921c305822ff20e8521fe105
toastedjets <58760267+toastedjets@users.noreply.github.com>
2025-07-18 11:48:39 -0400

    Do exercise 9

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let sum = 0;
+    
+    if (!Number.isInteger(start) || !Number.isInteger(end) || start < 0)
+        return "ERROR";
+    
+    else if (start < end) {
+        for (let i = start; i <= end; i++)
+            sum += i;
+    }
 
+    else if (start > end) {
+        for (let i = start; i >= end; i--)
+            sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
