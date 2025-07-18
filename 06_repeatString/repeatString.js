1dfef4e4522a1d4a557eb1c7cc89b994a89a7a08
toastedjets <58760267+toastedjets@users.noreply.github.com>
2025-07-18 10:21:31 -0400

    Do exercise 6

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, repeat) {
+    let strings = "";
+    if (repeat < 0)
+        return "ERROR";
+    else for (let i = 0; i < repeat; i++) {
+        strings += string;
+    }
+    
+    return strings;
 };
 
 // Do not edit below this line
