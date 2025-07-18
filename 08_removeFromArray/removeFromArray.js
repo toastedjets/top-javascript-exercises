95ef4d5ab1d20b70e196ac959511fe144ca89c10
toastedjets <58760267+toastedjets@users.noreply.github.com>
2025-07-18 11:35:42 -0400

    Do exercise 8

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...items) {
+    const newArray = [];
+
+    array.forEach((item) => {
+        if (!items.includes(item))
+            newArray.push(item);
+    });
+    
+    return newArray;
 };
 
 // Do not edit below this line
