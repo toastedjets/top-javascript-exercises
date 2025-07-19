72f91a0f9ec0a015b89933f2e35e62bc1f89320c
toastedjets <58760267+toastedjets@users.noreply.github.com>
2025-07-19 09:35:51 -0400

    Do exercise 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  const celsius = (temperature - 32) * 5 / 9;
+  const rounded = Math.round(celsius * 10) / 10;
+  return rounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  const fahrenheit = (temperature * 9 / 5) + 32;
+  const rounded = Math.round(fahrenheit * 10) / 10;
+  return rounded;
 };
 
 // Do not edit below this line
